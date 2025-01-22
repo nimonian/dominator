c55ea74d94f61cf395c58cd94baf276ad0481082
reuben longley <reuben.longley@clarionhg.com>
2025-01-22 11:05:55 +0000

    Demo&Colorbox

--- src/colorbox/script.js
+++ src/colorbox/script.js
@@ -1,1 +1,6 @@
 // TODO: add an event listener to the .box element that toggles the .red class
+const box = document.querySelector('.box')
+
+box.addEventListener('click', () => {
+    box.classList.toggle('red');
+});
